9c4f89856b150f9b0daf0e55340979df9b13a907
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    config: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -17,3 +17,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ThemePicker \
     ThemesStub
+    
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
