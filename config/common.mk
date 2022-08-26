82fbbe19bea6bda413b63fe209ae6aa5f6434f58
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2022-08-26 13:00:38 +0530

    vendor: Build Pixel Charging Animation for all
    No flag needed now
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -99,11 +99,9 @@ $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(INFINITY_BU
 endif
 
 # Charger
-ifeq ($(TARGET_USE_PIXEL_CHARGER),true)
 PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
-endif
 
 # Config
 PRODUCT_PACKAGES += \
