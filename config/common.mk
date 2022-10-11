9af3d815f8efc8219a56cd30855e48e2eb738165
Oliver Scott <olivercscott@gmail.com>
2022-10-11 20:39:01 +0200

    Build BatteryStatsViewer
    Change-Id: I1f712533f1914f7155f7a6657e3fa2095b4ea83a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -153,6 +153,10 @@ USE_DEX2OAT_DEBUG := false
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# BatteryStatsViewer
+PRODUCT_PACKAGES += \
+     BatteryStatsViewer
+
 # Bootanimation
 include vendor/infinity/config/bootanimation.mk
 
