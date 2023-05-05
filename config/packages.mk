871874273875aae9e74dfcc1de373b4786b27714
Luofan Chen <clfbbn@gmail.com>
2023-05-05 10:54:14 +0800

    vendor: Implement Lineage Health Service [3/3]
    config: Copy Lineage health permissions
    
    Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd

--- config/packages.mk
+++ config/packages.mk
@@ -14,3 +14,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/infinity/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
