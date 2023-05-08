f7d8b49523ec4137413bd6c426939ebbdc4ccd43
arnodorian-r <59914963+arnodorian-r@users.noreply.github.com>
2023-05-08 04:35:06 +0000

    soong: extend support for oppo camera
    Change-Id: Ic24a503298587f55d0acff2381c98093c51daa87
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -59,7 +59,8 @@ SOONG_CONFIG_infinityGlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    uses_nothing_camera
+    uses_nothing_camera \
+    uses_oppo_camera
 
 SOONG_CONFIG_NAMESPACES += infinityNvidiaVars
 SOONG_CONFIG_infinityNvidiaVars += \
@@ -97,6 +98,7 @@ SOONG_CONFIG_infinityGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PA
 SOONG_CONFIG_infinityGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 SOONG_CONFIG_infinityGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 SOONG_CONFIG_infinityGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_infinityGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
