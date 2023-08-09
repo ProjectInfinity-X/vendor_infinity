e92e3f64631329f410d87fbc20cafc5563ab8cb9
jhenrique09 <jhsv09@gmail.com>
2023-08-09 22:29:23 +0000

    vendor: Drop SimpleDeviceConfig
    Change-Id: Iba7e9e042b037f1147647608de9ab03a21fa00f9

--- config/common.mk
+++ config/common.mk
@@ -133,10 +133,6 @@ PRODUCT_COPY_FILES += \
     vendor/infinity/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 endif
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
-
 # Extra tools in infinity
 PRODUCT_PACKAGES += \
     bash \
