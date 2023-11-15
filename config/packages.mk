a916edcfe9d9b715010063f01aaea30b5a1baab6
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Add AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -3,3 +3,13 @@ PRODUCT_PACKAGES += \
     GameSpace \
     BatteryStatsViewer \
     ExactCalculator
+
+# Face Unlock
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
