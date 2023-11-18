40eff80afa9e8a0a9bf34fe1051be0f0772ad4e8
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    vendor: Enable Cloned App
    * Also fix some path includes for our use
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Iaaa61ac359f18b6569169be162a887b941b287bb

--- config/common.mk
+++ config/common.mk
@@ -45,6 +45,10 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     is_expressive_design_enabled=true
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/infinity/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-infinity-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-infinity-product.xml
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/infinity/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
