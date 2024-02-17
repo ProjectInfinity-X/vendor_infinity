da220f9291ae4d3dae522097adeafccc5cd638c9
LuK1337 <priv.luk@gmail.com>
2024-02-17 18:27:57 +0100

    config: Ship EmergencyInfo on WiFi only tablets
    Change-Id: I89e16f716076d5351949c0bb13f55fc4eaeee4f5
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -3,4 +3,6 @@ $(call inherit-product, vendor/infinity/config/common.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    androidx.window.extensions \
+    androidx.window.extensions
+
+$(call inherit-product, vendor/infinity/config/wifionly.mk)
