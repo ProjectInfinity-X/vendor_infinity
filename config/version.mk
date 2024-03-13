61b54a2964e4c8da50c00a0b55dc2f670b4bb975
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    version: Sign every build variant but eng

--- config/version.mk
+++ config/version.mk
@@ -59,7 +59,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.infinity.maintainer=$(INFINITY_MAINTAINER)
 
 # Signing
-ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/infinity/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/infinity/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
