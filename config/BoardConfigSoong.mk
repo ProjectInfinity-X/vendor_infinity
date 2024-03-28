abeaa1e57e2f8a3c71ec38257b4ff3b0be15f130
Jake Weinstein <jake@aospa.co>
2024-03-28 10:29:51 +0000

    soong: Add camera package name
    Use TARGET_CAMERA_PACKAGE_NAME to specify
    the package name.
    
    Suggested-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I9768d0ac40517b6555fd0606b299760f5baed71b
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -55,7 +55,8 @@ SOONG_CONFIG_infinityGlobalVars += \
     uses_egl_display_array \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
-    needs_camera_boottime
+    needs_camera_boottime \
+    target_camera_package_name
 
 SOONG_CONFIG_NAMESPACES += infinityNvidiaVars
 SOONG_CONFIG_infinityNvidiaVars += \
@@ -89,6 +90,7 @@ SOONG_CONFIG_infinityGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_
 SOONG_CONFIG_infinityGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_infinityGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_infinityGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_infinityGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
