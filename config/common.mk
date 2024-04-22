ProjectBlaze/vendor_blaze

config: Set default notification sound
Signed-off-by: rdx420 <padraramesh420@gmail.com>
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/common.mk
@@ -228,4 +228,5 @@ endif
 
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=vibe.ogg
+    ro.config.ringtone=vibe.ogg \
+    ro.config.notification_sound=Pikachu.ogg

ProjectInfinity-X/vendor_infinity

config/common.mk
@@ -199,4 +199,5 @@ include vendor/infinity/config/version.mk
 
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=vibe.ogg
+    ro.config.ringtone=vibe.ogg \
+    ro.config.notification_sound=Ping.ogg