hentaiOS/platform_vendor_hentai

animations: Import pixel off-charging animation
Test: Verify charging animation.

Change-Id: I70dacfedbd6ab6234b9020c929cfe8d4c22fe661
Signed-off-by: Dyneteve <dyneteve@hentaios.com>

config/common.mk
@@ -70,6 +70,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
 # Inherit from lewdboratory config
 $(call inherit-product-if-exists, vendor/lewdboratory/packages.mk)
 
+# Inherit from animations config
+$(call inherit-product, vendor/hentai/config/animations.mk)
+
 # Inherit from apex config
 $(call inherit-product, vendor/hentai/config/apex.mk)
 

ProjectInfinity-X/vendor_infinity

config/common.mk
@@ -156,11 +156,6 @@ PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 PRODUCT_PACKAGES += \
     BtHelper
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
-
 # Call Recording
 TARGET_CALL_RECORDING_SUPPORTED ?= true
 ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)
@@ -273,6 +268,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 include vendor/infinity/config/version.mk
 include vendor/infinity/config/packages.mk
 $(call inherit-product, vendor/infinity/config/clocks.mk)
+$(call inherit-product, vendor/infinity/config/animations.mk)
 
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \