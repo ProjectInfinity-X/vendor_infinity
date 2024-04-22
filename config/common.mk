Evolution-X-AOSP/vendor_evolution

vendor: Import clocks package from Android 14 QPR1
Change-Id: I0025fe6f12d68af8f4a8f4b256bd82822d37efdf

Co-authored-by: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -279,6 +279,9 @@ else ifneq ($(TARGET_USES_LEGACY_BOOTANIMATION), true)
 $(call inherit-product, vendor/evolution/config/bootanimation.mk)
 endif
 
+# Clocks
+$(call inherit-product, vendor/evolution/config/clocks.mk)
+
 # Inherit from fonts config
 $(call inherit-product, vendor/evolution/config/fonts.mk)
 

ProjectInfinity-X/vendor_infinity

config/common.mk
@@ -272,6 +272,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 include vendor/infinity/config/version.mk
 include vendor/infinity/config/packages.mk
+$(call inherit-product, vendor/infinity/config/clocks.mk)
 
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \