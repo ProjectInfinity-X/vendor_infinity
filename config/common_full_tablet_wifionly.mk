LineageOS/android_vendor_lineage

config: Ship EmergencyInfo on WiFi only tablets
Change-Id: I89e16f716076d5351949c0bb13f55fc4eaeee4f5

config/common_full_tablet_wifionly.mk
@@ -10,3 +10,5 @@ PRODUCT_PACKAGES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
+
+$(call inherit-product, vendor/lineage/config/wifionly.mk)

ProjectInfinity-X/vendor_infinity

Signed-off-by: Kanishk <kanishkthederp@gmail.com>

config/common_full_tablet_wifionly.mk
@@ -3,4 +3,6 @@ $(call inherit-product, vendor/infinity/config/common.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    androidx.window.extensions \
+    androidx.window.extensions
+
+$(call inherit-product, vendor/infinity/config/wifionly.mk)