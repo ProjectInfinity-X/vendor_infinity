ij-project/vendor_voltage

vendor: add support for WiFi only tablets
Signed-off-by: Kanishk <kanishkthederp@gmail.com>

config/common_full_tablet_wifionly.mk
@@ -1,0 +1,2 @@
+# Inherit common VoltageOS stuff
+$(call inherit-product, vendor/voltage/config/common.mk)

ProjectInfinity-X/vendor_infinity

config/common_full_tablet_wifionly.mk
@@ -1,0 +1,2 @@
+# Inherit common Infinity stuff
+$(call inherit-product, vendor/infinity/config/common.mk)