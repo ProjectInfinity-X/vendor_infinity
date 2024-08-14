8d820a6a262ffa54e3755978f8ea917bf5ad938a
nift4 <nift4@protonmail.com>
2024-08-14 16:24:35 +0800

    config: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -38,11 +38,7 @@ include vendor/extras/prebuilts.mk
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/infinity/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/infinity/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/infinity/prebuilt/common/bin/50-infinity.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-infinity.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-infinity.sh
+    vendor/infinity/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
