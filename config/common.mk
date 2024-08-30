20f5d36d39a43abdb721874ef0780f9200a3bc2f
ShevT <evgeny.shishkov@gmail.com>
2024-08-30 12:38:33 +0300

    Adding an option to build DeviceAsWebcam
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- config/common.mk
+++ config/common.mk
@@ -146,6 +146,15 @@ endif
 # Disable dex2oat debug
 USE_DEX2OAT_DEBUG := false
 
+# DeviceAsWebcam
+ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
+    PRODUCT_PACKAGES += \
+        DeviceAsWebcam
+
+    PRODUCT_VENDOR_PROPERTIES += \
+        ro.usb.uvc.enabled=true
+endif
+
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
