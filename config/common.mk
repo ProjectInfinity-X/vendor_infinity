247888371adfe831b47717ebf003e005cae7cc1f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-29 13:15:23 +0530

    vendor: Build LMO Freeform app and sidebar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -238,6 +238,11 @@ PRODUCT_PACKAGES += \
 # BatteryStatsViewer
 PRODUCT_PACKAGES += \
     BatteryStatsViewer
+    
+# Freeform
+PRODUCT_PACKAGES += \
+    LMOFreeform \
+    LMOFreeformSidebar
 
 # Storage manager
 PRODUCT_SYSTEM_PROPERTIES += \
