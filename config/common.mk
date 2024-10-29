b164d18331a1e375ebaf6142a41419d581eb5721
Tejas Singh <tejassingh649@rediffmail.com>
2024-10-29 13:57:02 +0530

    config: Warn if Google Telephony is intended to be shipped without GAPPS

--- config/common.mk
+++ config/common.mk
@@ -172,6 +172,12 @@ PRODUCT_PACKAGES += \
     UpdaterGMSOverlay
 endif
 
+ifeq ($(TARGET_BUILD_GOOGLE_TELEPHONY),true)
+ifneq ($(WITH_GAPPS),true)
+$(warning TARGET_BUILD_GOOGLE_TELEPHONY is declared without declaration of WITH_GAPPS to true, hence it will create no impact)
+endif
+endif
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
