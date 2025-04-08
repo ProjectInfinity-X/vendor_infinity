a3589a2e3de16a6ba5d1dec6e7a957864742360a
Tejas Singh <tejassingh649@rediffmail.com>
2025-04-09 02:29:49 +0530

    config: Build su for all

--- config/common.mk
+++ config/common.mk
@@ -268,15 +268,12 @@ PRODUCT_PACKAGES += \
 # Root
 PRODUCT_PACKAGES += \
     adb_root
-ifneq ($(TARGET_BUILD_VARIANT),user)
-ifeq ($(WITH_SU),true)
+
 PRODUCT_PACKAGES += \
     su
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/xbin/su
-endif
-endif
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
