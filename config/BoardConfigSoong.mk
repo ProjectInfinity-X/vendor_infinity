35b8834541d67f6f35a4ea59f4ca5fb9ebec5c7b
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-04-28 10:27:23 +0545

    soong: Move camera related configs to select()
    Change-Id: I102a3c94f3b3facd123c238c0530e179794fcf1d

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,27 +28,28 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += infinityGlobalVars
 SOONG_CONFIG_infinityGlobalVars += \
-    uses_miui_camera \
-    camera_needs_client_info_lib \
-    target_libcameraservice_ext_lib \
-    target_camera_package_name
+    camera_needs_client_info_lib
 
 # Soong bool variables
-SOONG_CONFIG_infinityGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_infinityGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
-SOONG_CONFIG_infinityGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_infinityGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
-
-# Set default values
-TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
-
-# Soong value variables
-SOONG_CONFIG_infinityGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(warning TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved)
     $(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
+endif
+
+ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
+    $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
+endif
+
+ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
+    $(call soong_config_set,camera,package_name,$(TARGET_CAMERA_PACKAGE_NAME))
+endif
+
+ifneq ($(TARGET_USES_MIUI_CAMERA),)
+    $(call soong_config_set,camera,uses_miui_camera,$(TARGET_USES_MIUI_CAMERA))
 endif
 
 # Vendor init
