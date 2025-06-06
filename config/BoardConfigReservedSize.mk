LineageOS/android_vendor_lineage

config: BoardConfigReservedSize: Disable via WITHOUT_RESERVED_SIZE=true
Test: WITHOUT_RESERVED_SIZE=true get_build_var BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT
Test: WITH_GMS=true get_build_var BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT
Test: get_build_var BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT
Change-Id: I54dfce46c57c4265954ecbdee3ed9b9f2fc18efe

config/BoardConfigReservedSize.mk
@@ -5,7 +5,7 @@ ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
     BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE ?= true
 endif
 
-ifneq ($(WITH_GMS),true)
+ifeq (,$(filter true, $(WITHOUT_RESERVED_SIZE) $(WITH_GMS)))
     BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT ?= -1
 
     ifeq ($(BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE),true)

ProjectInfinity-X/vendor_infinity

Test: WITHOUT_RESERVED_SIZE=true \
      get_build_var BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT
Test: WITH_GMS=true \
Test: WITHOUT_RESERVED_SIZE=true WITH_GMS=true \

config/BoardConfigReservedSize.mk
@@ -5,7 +5,7 @@ ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
     BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE ?= true
 endif
 
-ifneq ($(WITH_GAPPS),true)
+ifeq (,$(filter true, $(WITHOUT_RESERVED_SIZE) $(WITH_GAPPS)))
     BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT ?= -1
 
     ifeq ($(BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE),true)