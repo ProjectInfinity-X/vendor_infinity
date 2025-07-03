968c5e4c488e9d53baf3f51cc795eded1080e990
Ghosuto <clash.raja10@gmail.com>
2025-07-03 16:18:12 +0000

    fixup! common: Disable art debug builds

--- config/common.mk
+++ config/common.mk
@@ -119,7 +119,7 @@ ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 include vendor/infinity/config/lineage_sdk_common.mk
 endif
 
-ART_BUILD_TARGET_NDEBUG := false
+ART_BUILD_HOST_DEBUG := false
 ART_BUILD_TARGET_DEBUG := false
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 WITH_DEXPREOPT_DEBUG_INFO := false
