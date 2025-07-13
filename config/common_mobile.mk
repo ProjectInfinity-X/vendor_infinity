414c3e70613888a4d8366fbcd1d145b7c6dae6fb
Tejas Singh <tejassingh649@rediffmail.com>
2025-07-13 14:57:55 +0530

    telephony: Remove duplicate default ringtone definition

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -8,11 +8,6 @@ include vendor/infinity/config/aosp_audio.mk
 # Include Lineage audio files
 include vendor/infinity/config/lineage_audio.mk
 
-# Default notification/alarm sounds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
-
 # Apps
 PRODUCT_PACKAGES += \
     AvatarPicker \
